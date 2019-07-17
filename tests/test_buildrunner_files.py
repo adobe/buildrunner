e16fdea14d4cdf97b41c7659632555f4d64fd8e8
Clint Savage <clsavage@adobe.com>
2019-07-17 12:23:13 -0600

    Mock out config files, add alias mount test

--- tests/test_buildrunner_files.py
+++ tests/test_buildrunner_files.py
@@ -4,7 +4,9 @@ import os
 import shutil
 import tempfile
 import unittest
+import mock
 
+import buildrunner
 from buildrunner import cli
 
 
@@ -12,22 +14,27 @@ class Test_buildrunner_files(unittest.TestCase):
 
 
     def test_buildrunner_files(self):
+
         test_dir_path = os.path.realpath(os.path.dirname(__file__))
         test_dir = os.path.basename(os.path.dirname(__file__))
         top_dir_path = os.path.realpath(os.path.dirname(test_dir_path))
 
-        br_files = sorted([f for f in os.listdir(test_dir) if f.endswith('.yaml')])
-        for br_file in br_files:
-            print('\n>>>> Testing Buildrunner file: {0}'.format(br_file))
-            self.assertEqual(
-                cli.main([
-                    'buildrunner-test',
-                    '-d', top_dir_path,
-                    '-f', os.path.join(test_dir, br_file),
-                    '--push',
-                ]),
-                os.EX_OK,
-            )
+        with mock.patch.object(buildrunner, "DEFAULT_GLOBAL_CONFIG_FILES",
+                               ['{0}/test-data/etc-buildrunner.yaml'.format(test_dir_path),
+                                '{0}/test-data/dot-buildrunner.yaml'.format(test_dir_path)]):
+
+            br_files = sorted([f for f in os.listdir(test_dir) if f.endswith('.yaml')])
+            for br_file in br_files:
+                print('\n>>>> Testing Buildrunner file: {0}'.format(br_file))
+                self.assertEqual(
+                    cli.main([
+                        'buildrunner-test',
+                        '-d', top_dir_path,
+                        '-f', os.path.join(test_dir, br_file),
+                        '--push',
+                    ]),
+                    os.EX_OK,
+                )
 
 
 if __name__ == '__main__':
