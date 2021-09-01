464d40912bdb3e3bfed74fd1fe4c930749572476
saville <saville@adobe.com>
2021-09-01 14:53:41 -0600

    Rework tests to run a test for each file

--- tests/test_buildrunner_files.py
+++ tests/test_buildrunner_files.py
@@ -1,55 +1,58 @@
 import os
+import pytest
 import sys
-import unittest
+from typing import List, Optional, Tuple
 
-import tests.test_runner as tr
+from tests import test_runner
 
 
-class Test_buildrunner_files(unittest.TestCase):
+test_dir_path = os.path.realpath(os.path.dirname(__file__))
+test_dir = os.path.basename(os.path.dirname(__file__))
+top_dir_path = os.path.realpath(os.path.dirname(test_dir_path))
 
-    def _get_test_args(self, br_file):
-        if br_file == 'test-timeout.yaml':
-            # Set a short timeout here for the timeout test
-            return ['-t', '15']
-        # No additional args for this test file
-        return None
 
-    def test_buildrunner_files(self):
+def _get_test_args(file_name: str) -> Optional[List[str]]:
+    if file_name == 'test-timeout.yaml':
+        # Set a short timeout here for the timeout test
+        return ['-t', '15']
 
-        test_dir_path = os.path.realpath(os.path.dirname(__file__))
-        test_dir = os.path.basename(os.path.dirname(__file__))
-        top_dir_path = os.path.realpath(os.path.dirname(test_dir_path))
+    # No additional args for this test file
+    return None
 
-        br_files = sorted([f for f in os.listdir(test_dir) if f.startswith('test-') and f.endswith('.yaml')])
-        for br_file in br_files:
-            print(f'\n>>>> Testing Buildrunner file: {br_file}')
-            args = self._get_test_args(br_file)
-            command_line = [
-                'buildrunner-test',
-                '-d', top_dir_path,
-                '-f', os.path.join(test_dir, br_file),
-                '--push',
+
+def _get_exit_code(file_name: str) -> int:
+    if file_name.startswith('test-xfail'):
+        return os.EX_CONFIG
+    return os.EX_OK
+
+
+def _get_test_runs() -> List[Tuple[str, Optional[List[str]], int]]:
+    file_names = sorted([
+        file_name for file_name in os.listdir(test_dir)
+        if file_name.startswith('test-') and file_name.endswith('.yaml')
+    ])
+    return [(file_name, _get_test_args(file_name), _get_exit_code(file_name)) for file_name in file_names]
+
+
+@pytest.mark.parametrize('file_name, args, exit_code', _get_test_runs())
+def test_buildrunner_file(file_name, args, exit_code):
+    print(f'\n>>>> Testing Buildrunner file: {file_name}')
+    command_line = [
+        'buildrunner-test',
+        '-d', top_dir_path,
+        '-f', os.path.join(test_dir, file_name),
+        # Do not push in tests
+    ]
+    if args:
+        command_line.extend(args)
+
+    assert exit_code == \
+        test_runner.run_tests(
+            command_line,
+            master_config_file = f'{test_dir_path}/test-data/etc-buildrunner.yaml',
+            global_config_files = [
+                f'{test_dir_path}/test-data/etc-buildrunner.yaml',
+                f'{test_dir_path}/test-data/dot-buildrunner.yaml',
             ]
-            if args:
-                command_line.extend(args)
+        )
 
-            if br_file.startswith('test-xfail'):
-                assert_func = self.assertNotEqual
-            else:
-                assert_func = self.assertEqual
-
-            assert_func(
-                tr.run_tests(
-                    command_line,
-                    master_config_file = f'{test_dir_path}/test-data/etc-buildrunner.yaml',
-                    global_config_files = [
-                        f'{test_dir_path}/test-data/etc-buildrunner.yaml',
-                        f'{test_dir_path}/test-data/dot-buildrunner.yaml',
-                    ]
-                ),
-                os.EX_OK,
-            )
-
-
-if __name__ == '__main__':
-    unittest.main(argv=sys.argv)
