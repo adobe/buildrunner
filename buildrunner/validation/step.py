da4b58f8fec642b0565230f4a8b3373bccf94cdf
Shane Brown <shanejbrown@gmail.com>
2023-10-13 17:02:37 -0600

    Upgrade pydantic and python-on-whales

--- buildrunner/validation/step.py
+++ buildrunner/validation/step.py
@@ -22,22 +22,22 @@ class StepPypiPush(BaseModel, extra='forbid'):
 class Artifact(BaseModel):
     """ Artifact model """
     # Intentionally loose restrictions
-    format: Optional[str]
-    type: Optional[Any]
-    compression: Optional[str]
-    push: Optional[bool]
+    format: Optional[str] = None
+    type: Optional[Any] = None
+    compression: Optional[str] = None
+    push: Optional[bool] = None
 
 
 class StepBuild(BaseModel, extra='forbid'):
     """ Build model within a step """
-    path: Optional[str]
-    dockerfile: Optional[str]
-    pull: Optional[bool]
-    platform: Optional[str]
-    platforms: Optional[List[str]]
-    inject: Optional[Dict[str, Optional[str]]]
-    no_cache: Optional[bool] = Field(alias='no-cache')
-    buildargs: Optional[Dict[str, Any]]
+    path: Optional[str] = None
+    dockerfile: Optional[str] = None
+    pull: Optional[bool] = None
+    platform: Optional[str] = None
+    platforms: Optional[List[str]] = None
+    inject: Optional[Dict[str, Optional[str]]] = None
+    no_cache: Optional[bool] = Field(alias='no-cache', default=None)
+    buildargs: Optional[Dict[str, Any]] = None
 
 
 class RunAndServicesBase(BaseModel):
@@ -45,74 +45,74 @@ class RunAndServicesBase(BaseModel):
     Base model for Run and Service
     which has several common fields
     """
-    image: Optional[str]
-    cmd: Optional[str]
+    image: Optional[str] = None
+    cmd: Optional[str] = None
     # Intentionally loose restrictions
-    provisioners: Optional[Dict[str, str]]
-    shell: Optional[str]
-    cwd: Optional[str]
-    user: Optional[str]
-    hostname: Optional[str]
-    dns: Optional[List[str]]
-    dns_search: Optional[str]
-    extra_hosts: Optional[Dict[str, str]]
-    env: Optional[Dict[str, Optional[str]]]
-    files: Optional[Dict[str, str]]
-    volumes_from: Optional[List[str]]
-    ports: Optional[Dict[int, Optional[int]]]
-    pull: Optional[bool]
-    systemd: Optional[bool]
-    containers: Optional[List[str]]
-    caches: Optional[Dict[str, Union[str, List[str]]]]
+    provisioners: Optional[Dict[str, str]] = None
+    shell: Optional[str] = None
+    cwd: Optional[str] = None
+    user: Optional[str] = None
+    hostname: Optional[str] = None
+    dns: Optional[List[str]] = None
+    dns_search: Optional[str] = None
+    extra_hosts: Optional[Dict[str, str]] = None
+    env: Optional[Dict[str, Optional[str]]] = None
+    files: Optional[Dict[str, str]] = None
+    volumes_from: Optional[List[str]] = None
+    ports:  Optional[Dict[int, Optional[int]]] = None
+    pull: Optional[bool] = None
+    systemd: Optional[bool] = None
+    containers: Optional[List[str]] = None
+    caches: Optional[Dict[str, Union[str, List[str]]]] = None
 
 
 class Service(RunAndServicesBase, extra='forbid'):
     """ Service model """
-    build: Optional[Union[StepBuild, str]]
-    wait_for: Optional[List[Any]]
-    inject_ssh_agent: Optional[bool] = Field(alias='inject-ssh-agent')
+    build: Optional[Union[StepBuild, str]] = None
+    wait_for: Optional[List[Any]] = None
+    inject_ssh_agent: Optional[bool] = Field(alias='inject-ssh-agent', default=None)
     # Not sure if this is valid, but it is in a test file
     # Didn't use StepRun because of the potential to have a infinitely nested model
-    run: Optional[Any]
+    run: Optional[Any] = None
 
 
 class StepRun(RunAndServicesBase, extra='forbid'):
     """ Run model within a step """
-    xfail: Optional[bool]
-    services: Optional[Dict[str, Service]]
-    cmds: Optional[List[str]]
-    ssh_keys: Optional[List[str]] = Field(alias='ssh-keys')
-    artifacts: Optional[Dict[str, Optional[Artifact]]]
-    platform: Optional[str]
-    cap_add: Optional[Union[str, List[str]]]
-    privileged: Optional[bool]
-    post_build: Optional[Union[str, Dict[str, Any]]] = Field(alias='post-build')
-    no_cache: Optional[bool] = Field(alias='no-cache')
+    xfail: Optional[bool] = None
+    services: Optional[Dict[str, Service]] = None
+    cmds: Optional[List[str]] = None
+    ssh_keys: Optional[List[str]] = Field(alias='ssh-keys', default=None)
+    artifacts: Optional[Dict[str, Union[Artifact, None]]] = None
+    platform: Optional[str] = None
+    cap_add: Optional[Union[str, List[str]]] = None
+    privileged: Optional[bool] = None
+    post_build: Optional[Union[str, Dict[str, Any]]] = Field(alias='post-build', default=None)
+    no_cache: Optional[bool] = Field(alias='no-cache', default=None)
 
 
 class StepRemote(BaseModel, extra='forbid'):
     """ Remote model within a step """
     # Not sure if host is optional or required
-    host: Optional[str]
+    host: Optional[str] = None
     cmd: str
-    artifacts: Optional[Dict[str, Optional[Artifact]]]
+    artifacts: Optional[Dict[str, Optional[Artifact]]] = None
 
 
 class StepPushCommitDict(BaseModel, extra='forbid'):
     """ Push model within a step """
     repository: str
-    tags: Optional[List[str]]
+    tags: Optional[List[str]] = None
 
 
 class Step(BaseModel, extra='forbid'):
     """ Step model """
-    build: Optional[Union[StepBuild, str]]
-    push: Optional[Union[StepPushCommitDict, List[Union[str, StepPushCommitDict]], str]]
-    commit: Optional[Union[StepPushCommitDict, List[Union[str, StepPushCommitDict]], str]]
-    remote: Optional[StepRemote]
-    run: Optional[StepRun]
-    depends: Optional[List[str]]
-    pypi_push: Optional[Union[StepPypiPush, str]] = Field(alias='pypi-push')
+    build: Optional[Union[StepBuild, str]] = None
+    push: Optional[Union[StepPushCommitDict, List[Union[str, StepPushCommitDict]], str]] = None
+    commit: Optional[Union[StepPushCommitDict, List[Union[str, StepPushCommitDict]], str]] = None
+    remote: Optional[StepRemote] = None
+    run: Optional[StepRun] = None
+    depends: Optional[List[str]] = None
+    pypi_push: Optional[Union[StepPypiPush, str]] = Field(alias='pypi-push', default=None)
 
     def is_multi_platform(self):
         """
