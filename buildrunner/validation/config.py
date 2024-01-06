3ec63d0af6f5ee7ac182f3c34e3d1e3221e7b9b8
Shane Brown <shanejbrown@gmail.com>
2024-01-05 18:09:57 -0700

    Implement multi-platform re-tag config validation

--- buildrunner/validation/config.py
+++ buildrunner/validation/config.py
@@ -6,6 +6,7 @@ NOTICE: Adobe permits you to use, modify, and distribute this file in accordance
 with the terms of the Adobe license agreement accompanying it.
 """
 
+import os
 from typing import Any, Dict, List, Optional, Set, Union
 
 # pylint: disable=no-name-in-module
@@ -14,7 +15,9 @@ from pydantic import BaseModel, Field, field_validator, ValidationError
 from buildrunner.docker import multiplatform_image_builder
 from buildrunner.validation.errors import Errors, get_validation_errors
 from buildrunner.validation.step import Step, StepPushCommitDict
+from buildrunner.validation.step_images_info import StepImagesInfo
 
+RETAG_ERROR_MESSAGE = "Multi-platform build steps cannot re-tag images. The following images are re-tagged:"
 
 class Config(BaseModel, extra="forbid"):
     """Top level config model"""
@@ -110,18 +113,19 @@ class Config(BaseModel, extra="forbid"):
                         name = f"{name}:latest"
 
                 if isinstance(push, StepPushCommitDict):
-                    names = [f"{push.repository}:{tag}" for tag in push.tags]
+                    if not push.tags:
+                        name = f'{push.repository}:latest'
+                    else:
+                        names = [f"{push.repository}:{tag}" for tag in push.tags]
 
                 if names is not None:
                     for current_name in names:
                         if current_name in mp_push_tags:
-                            # raise ValueError(f'Cannot specify duplicate tag {current_name} in build step {step_name}')
                             raise ValueError(
                                 f"Cannot specify duplicate tag {current_name} in build step {step_name}"
                             )
 
                 if name is not None and name in mp_push_tags:
-                    # raise ValueError(f'Cannot specify duplicate tag {name} in build step {step_name}')
                     raise ValueError(
                         f"Cannot specify duplicate tag {name} in build step {step_name}"
                     )
@@ -131,6 +135,134 @@ class Config(BaseModel, extra="forbid"):
 
                 if update_mp_push_tags and name is not None:
                     mp_push_tags.add(name)
+
+        def get_source_image(step: Step) -> Optional[List[str]]:
+            """
+            Get the source image from the step.build and/or step.run.image
+
+            Args:
+                step (Step): Build step
+
+            Returns:
+                Optional[str]: Source images
+            """
+            dockerfile_text = None
+            src_image = []
+            is_multi_platform = False
+
+            # Gets the source image from build.dockerfile
+            if step.build and step.build.dockerfile:
+                if os.path.exists(step.build.dockerfile):
+                    with open(step.build.dockerfile, "r") as dockerfile:
+                        dockerfile_text = dockerfile.read()
+                else:
+                    # Dockerfile is defined as a string in the config file
+                    dockerfile_text = step.build.dockerfile
+
+                if not dockerfile_text:
+                    raise ValueError(
+                        f"There is an issue with the dockerfile {step.build.dockerfile}"
+                    )
+
+                for line in dockerfile_text.splitlines():
+                    if line.startswith("FROM"):
+                        image_name = line.replace("FROM ", "").strip()
+                        if ":" not in image_name:
+                            image_name = f"{image_name}:latest"
+                        src_image.append(image_name)
+
+                if not src_image:
+                    raise ValueError(
+                        f"There is an issue with the dockerfile {step.build.dockerfile}"
+                    )
+
+            # Get the source image from step.run.image
+            if step.run and step.run.image:
+                if ":" not in step.run.image:
+                    src_image.append(f"{step.run.image}:latest")
+                else:
+                    src_image.append(step.run.image)
+
+            if step.build and step.build.platforms:
+                is_multi_platform = True
+
+            return src_image, is_multi_platform
+
+        def get_destination_images(step: Step) -> Optional[List[str]]:
+            """
+            Get the destination images from step.push or step.commit
+
+            Args:
+                step (Step): Build step
+
+            Returns:
+                Optional[List[str]]: List of destination images
+            """
+            def get_images(pushcommmit: Union[StepPushCommitDict, str, List[Union[str, StepPushCommitDict]]]) -> List[str]:
+                images = []
+                if isinstance(pushcommmit, StepPushCommitDict):
+                    if pushcommmit.tags:
+                        for tag in pushcommmit.tags:
+                            images.append(f"{pushcommmit.repository}:{tag}")
+                    else:
+                        images.append(f"{pushcommmit.repository}:latest")
+                elif isinstance(pushcommmit, str):
+                    if ":" not in pushcommmit:
+                        images.append(f"{pushcommmit}:latest")
+                    else:
+                        images.append(pushcommmit)
+                elif isinstance(pushcommmit, list):
+                    for item in pushcommmit:
+                        if isinstance(item, str):
+                            images.append(item)
+                        elif isinstance(item, StepPushCommitDict):
+                            if item.tags:
+                                for tag in item.tags:
+                                    images.append(f"{item.repository}:{tag}")
+                            else:
+                                images.append(f"{item.repository}:latest")
+                        else:
+                            raise ValueError(f"Unknown type for step.push: {type(step.push)}")
+                return images
+
+            images = []
+            images.extend(get_images(step.push))
+            images.extend(get_images(step.commit))
+            return images
+
+        def validate_multiplatform_are_not_retagged():
+            """
+            Validate multi-platform are not re-tagged
+
+            Args:
+                mp_push_tags (Set[str]): Set of all tags used in multi-platform build steps
+
+            Raises:
+                ValueError | pydantic.ValidationError: If the config file is invalid
+            """
+            step_images = {}
+
+            # Iterate through each step and get information about the images
+            for step_name, step in vals.items():
+                src_image, is_multi_platform = get_source_image(step)
+                dst_images = get_destination_images(step)
+                step_images[step_name] = StepImagesInfo(
+                    source_image=src_image,
+                    dest_images=dst_images,
+                    is_multi_platform=is_multi_platform)
+
+            # Iterate through each step images and check for multi-platform re-tagging
+            retagged_images = []
+            for step_name, step_images_info in step_images.items():
+                other_steps_images_infos = [curr_step_images_info for curr_step_name, curr_step_images_info in step_images.items() if curr_step_name != step_name]
+                for other_step_info in other_steps_images_infos:
+                    for src_image in step_images_info.source_image:
+                        if src_image in other_step_info.dest_images and other_step_info.is_multi_platform:
+                            retagged_images.append(src_image)
+
+            if retagged_images:
+                raise ValueError(f"{RETAG_ERROR_MESSAGE} {retagged_images}")
+
 
         def validate_multi_platform_build(mp_push_tags: Set[str]):
             """
@@ -142,7 +274,7 @@ class Config(BaseModel, extra="forbid"):
             Raises:
                 ValueError | pydantic.ValidationError: If the config file is invalid
             """
-            # Iterate through each step
+            # Iterate through each step and validate multi-platform multi-platform steps
             for step_name, step in vals.items():
                 if step.is_multi_platform():
                     if step.build.platform is not None:
@@ -166,9 +298,17 @@ class Config(BaseModel, extra="forbid"):
                             f"import is not allowed in multi-platform build step {step_name}"
                         )
 
+                    if step.run:
+                        raise ValueError(
+                            f"run is not allowed with multi-platform build step {step_name}"
+                        )
+
                     # Check for valid push section, duplicate mp tags are not allowed
                     validate_push(step.push, mp_push_tags, step_name)
 
+            validate_multiplatform_are_not_retagged()
+
+        # Checks to see if there is a mutli-platform build step in the config
         has_multi_platform_build = False
         for step in vals.values():
             has_multi_platform_build = (
---
+++ tests/test_config_validation/test_multi_platform.py
@@ -0,0 +1,46 @@
+
+import yaml
+from buildrunner.validation.config import validate_config, Errors
+
+
+def test_no_run_with_multiplatform_build():
+    # Run in multi platform build is not supported
+    config_yaml = """
+    steps:
+        build-container-multi-platform:
+            build:
+                dockerfile: |
+                    FROM {{ DOCKER_REGISTRY }}/busybox:latest
+                platforms:
+                    - linux/amd64
+                    - linux/arm64/v8
+            push:
+                repository: user1/buildrunner-test-multi-platform
+                tags: []
+            run:
+                image: user1/buildrunner-test-multi-platform
+                cmd: echo "Hello World"
+    """
+    config = yaml.load(config_yaml, Loader=yaml.Loader)
+    errors = validate_config(**config)
+    assert isinstance(errors, Errors)
+    assert errors.count() == 1
+
+def test_no_run_with_single_build():
+    # Run in single platform build is supported
+    config_yaml = """
+    steps:
+        build-container-multi-platform:
+            build:
+                dockerfile: |
+                    FROM {{ DOCKER_REGISTRY }}/busybox:latest
+            push:
+                repository: user1/buildrunner-test-multi-platform
+                tags: []
+            run:
+                image: user1/buildrunner-test-multi-platform
+                cmd: echo "Hello World"
+    """
+    config = yaml.load(config_yaml, Loader=yaml.Loader)
+    errors = validate_config(**config)
+    assert errors is None
