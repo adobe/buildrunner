b3df82e3c363236b672411e31b55f3d068197c08
saville <saville@adobe.com>
2024-03-29 12:34:15 -0600

    Upgrade version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ import types
 from setuptools import setup, find_packages
 
 
-BASE_VERSION = "3.6"
+BASE_VERSION = "3.7"
 
 SOURCE_DIR = os.path.dirname(os.path.abspath(__file__))
 BUILDRUNNER_DIR = os.path.join(SOURCE_DIR, "buildrunner")
