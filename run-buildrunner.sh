6d987f4e14704e48979decda1e31f1768bab3889
Shane Brown <shanejbrown@gmail.com>
2025-03-03 11:24:30 -0700

    Add buildargs and import examples.

--- run-buildrunner.sh
+++ run-buildrunner.sh
@@ -3,4 +3,4 @@
 # This script will run the local buildrunner code from the root of the project
 # ./run-buildrunner.sh --help
 # ./run-buildrunner.sh -f examples/build/basic/buildrunner.yaml
-PYTHONPATH=. ./bin/buildrunner "$@"
+PYTHONPATH=. ./bin/buildrunner --cleanup-images "$@"
