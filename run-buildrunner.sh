1fd7392143601a7ee0673314ccaa6f3e0ae4a2aa
Shane Brown <shanejbrown@gmail.com>
2025-03-03 11:25:53 -0700

    Update run-buildrunner.sh

--- run-buildrunner.sh
+++ run-buildrunner.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # This script will run the local buildrunner code from the root of the project
-# ./run-buildrunner.sh --help
-# ./run-buildrunner.sh -f examples/build/basic/buildrunner.yaml
+#
+# To get help, run the following command:
+#   ./run-buildrunner.sh --help
+#
+# To run the buildrunner with a specific configuration file, run the following command:
+#   ./run-buildrunner.sh -f examples/build/basic/buildrunner.yaml
 PYTHONPATH=. ./bin/buildrunner --cleanup-images "$@"
